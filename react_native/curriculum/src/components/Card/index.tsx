8d14b19517d2d8808d8f198aaa60bc4ef118be3f
Leonardo Joao <leonardorsjoao@gmail.com>
2023-04-11 21:06:22 -0300

    [react_native][curriculum] Create card style component to separate styles

--- react_native/curriculum/src/components/Card/index.tsx
+++ react_native/curriculum/src/components/Card/index.tsx
@@ -6,7 +6,9 @@
  */
 
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {View, Text} from 'react-native';
+
+import styles from './style';
 
 function Card(props): JSX.Element {
   return (
@@ -27,33 +29,5 @@ function Card(props): JSX.Element {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  card_container: {
-    alignItems: 'center',
-    width: '100%',
-    marginTop: 30,
-  },
-  card: {
-    width: '65%',
-
-    padding: 10,
-    borderRadius: 5,
-    backgroundColor: '#F8F8FF',
-  },
-  card_header: {
-    marginTop: 0,
-  },
-  card_header_text: {
-    fontWeight: 'bold',
-  },
-  card_content: {
-    marginTop: 20,
-  },
-  card_content_text: {
-    color: '#939393',
-    marginBottom: 10,
-  },
-});
 
 export default Card;
