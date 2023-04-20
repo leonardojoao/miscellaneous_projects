f87bde0fe8835bae2d39a65377230a580f07b4ea
Leonardo Joao <leonardorsjoao@gmail.com>
2023-04-20 13:16:12 -0300

    [react_native][my_skills] Add fields and methods for recording skills

---
+++ react_native/my_skills/src/pages/Styles.tsx
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121005',
+    paddingTop: 20,
+    paddingHorizontal: 20,
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: 15,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 8,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 27,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
