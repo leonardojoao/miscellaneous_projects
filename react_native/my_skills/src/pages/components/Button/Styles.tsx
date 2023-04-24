b6d0b49e965bb6c0e573a545f5513e5d74788532
Leonardo Joao <leonardorsjoao@gmail.com>
2023-04-24 20:47:04 -0300

    [react_native][my_skills] Create button component

---
+++ react_native/my_skills/src/pages/components/Button/Styles.tsx
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 8,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 27,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
