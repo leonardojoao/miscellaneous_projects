e99af6a86f862f818ee6b8531946649907d849cf
Leonardo Joao <leonardorsjoao@gmail.com>
2023-04-24 21:30:31 -0300

    [react_native][my_skills] Create skillCard component to list skills

---
+++ react_native/my_skills/src/pages/components/SkillCard/Styles.tsx
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+const Styles = StyleSheet.create({
+  buttonSkill: {
+    backgroundColor: '#1F1E25',
+    padding: 15,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  textSkill: {
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default Styles;
