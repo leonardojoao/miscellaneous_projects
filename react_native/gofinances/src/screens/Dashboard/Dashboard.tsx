126ede6be32f1a82374ca1347363402c3d19e72b
Leonardo Joao <leonardorsjoao@gmail.com>
2023-05-01 19:09:44 -0300

    [react_native][gofinances] Create the base of the application

---
+++ react_native/gofinances/src/screens/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Container, Title} from './styles';
+
+function Dashboard(): JSX.Element {
+  return (
+    <Container>
+      <Title>Dasboard</Title>
+    </Container>
+  );
+}
+
+export default Dashboard;
