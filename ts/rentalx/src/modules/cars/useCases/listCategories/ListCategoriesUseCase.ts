0f118cb285a7044e00ff46b671882e7efca77379
Leonardo Joao <leonardorsjoao@gmail.com>
2023-07-10 15:08:13 -0300

    [ts][rentalx] Use controllers in category

---
+++ ts/rentalx/src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,14 @@
+import { Category } from '../../model/Category';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+class ListCategoriesUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute(): Category[] {
+    const categoriesList = this.categoriesRepository.list();
+
+    return categoriesList;
+  }
+}
+
+export { ListCategoriesUseCase };
