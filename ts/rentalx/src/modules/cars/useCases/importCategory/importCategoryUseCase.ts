4eb60da1d5d2a6549fececf50d939c21b0e9142a
Leonardo Joao <leonardorsjoao@gmail.com>
2023-07-11 11:40:45 -0300

    [ts][rentalx] Create route to upload file and save categories by csv file

---
+++ ts/rentalx/src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -0,0 +1,58 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+
+      const parseFile = parse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on('data', async (line: any) => {
+          const [name, description] = line;
+
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File | undefined): Promise<void> {
+    if (file) {
+      const categories = await this.loadCategories(file);
+      console.log(categories);
+
+      categories.map((category) => {
+        const { name, description } = category;
+
+        const existCategory = this.categoriesRepository.findByName(name);
+
+        if (!existCategory) {
+          this.categoriesRepository.create({ name, description });
+        }
+      });
+    }
+  }
+}
+
+export { ImportCategoryUseCase };
