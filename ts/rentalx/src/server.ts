1cf1b20a64e7a01797d2e035ccef4546249e49e4
Leonardo Joao <leonardorsjoao@gmail.com>
2023-07-11 13:43:39 -0300

    [ts][rentalx] Add swagger library and create documentation for post and get categories method

--- ts/rentalx/src/server.ts
+++ ts/rentalx/src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import { categoriesRoutes } from './routes/categories.routes';
 import { specificationsRoutes } from './routes/specification.routes';
+
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use('/categories', categoriesRoutes);
 app.use('/specifications', specificationsRoutes);
