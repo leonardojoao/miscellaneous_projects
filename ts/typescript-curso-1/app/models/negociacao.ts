008b268e1cf8c030757a3a66572985952a8da3cf
Leonardo Joao <leonardorsjoao@gmail.com>
2023-12-05 14:48:32 -0300

    [ts][typescript-curso-1] Refactor negociacao model

--- ts/typescript-curso-1/app/models/negociacao.ts
+++ ts/typescript-curso-1/app/models/negociacao.ts
@@ -1,19 +1,16 @@
 export class Negociacao {
-  private _data: Date;
-  private _quantidade: number;
-  private _valor: number;
-
-  constructor(_data: Date, quantidade: number, valor: number) {
-    this._data = _data;
-    this._quantidade = quantidade;
-    this._valor = valor;
-  }
+  constructor(
+    private _data: Date,
+    private readonly quantidade: number,
+    private readonly valor: number
+  ) {}
 
   get data(): Date {
-    return this._data;
+    const data = new Date(this._data.getTime());
+    return data;
   }
 
   get volume(): number {
-    return this._quantidade * this._valor;
+    return this.quantidade * this.valor;
   }
 }
