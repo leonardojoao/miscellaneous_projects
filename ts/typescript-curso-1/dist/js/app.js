ec5a8b17c2abcf7fc419b0ba825ed8588d5e88e5
Leonardo Joao <leonardorsjoao@gmail.com>
2023-12-05 14:50:54 -0300

    [ts][typescript-curso-1] Add logic in app component

--- ts/typescript-curso-1/dist/js/app.js
+++ ts/typescript-curso-1/dist/js/app.js
@@ -1,11 +1,7 @@
-// import { NegociacaoController } from './controllers/negociacao-controller.js';
-// const controller = new NegociacaoController();
-// const form = document.querySelector('.form');
-// form.addEventListener('submit', event => {
-//     event.preventDefault();
-//     controller.adiciona();
-// });
-import { Negociacao } from "./models/negociacao.js";
-const negociacao = new Negociacao(new Date(), 20, 10);
-console.log(negociacao);
-console.log('Aquiiu', 'aqui', negociacao.volume);
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoController();
+const form = document.querySelector(".form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    controller.adiciona();
+});
