c24772d552f353c7815b50da4849957dfae59853
Leonardo Joao <leonardorsjoao@gmail.com>
2023-12-21 12:08:30 -0300

    [ts][typescript-curso] Create formt data method

--- ts/typescript-curso/app/views/negociacoes-view.ts
+++ ts/typescript-curso/app/views/negociacoes-view.ts
@@ -19,7 +19,7 @@ export class NegociacoesView extends View<Negociacoes> {
             .map((negociacao) => {
               return `
               <tr>
-                <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                <td>${this.formatar(negociacao.data)}</td>
                 <td>${negociacao.quantidade}</td>
                 <td>${negociacao.valor}</td>
               </tr>
@@ -29,5 +29,9 @@ export class NegociacoesView extends View<Negociacoes> {
         </tbody>
       </table>
     `;
+  }
+
+  private formatar(data: Date): string {
+    return new Intl.DateTimeFormat().format(data);
   }
 }
