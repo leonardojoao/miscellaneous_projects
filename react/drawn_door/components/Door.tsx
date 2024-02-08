ba526d5412d76bdb06830abf29d190f23e77c1e1
Leonardo Joao <leonardorsjoao@gmail.com>
2024-02-08 20:20:49 -0300

    WIP

--- react/drawn_door/components/Door.tsx
+++ react/drawn_door/components/Door.tsx
@@ -11,14 +11,24 @@ export default function Door(props: DoorProps) {
   const selected = door.selected ? styles.selected : "";
 
   const changeSelected = (e) => props.onChange(door.select());
+  const open = (e) => {
+    e.stopPropagation();
+    props.onChange(door.open());
+  };
+
+  function renderDoor() {
+    return (
+      <div className={styles.door}>
+        <div className={styles.number}>{door.number}</div>
+        <div className={styles.doorknob} onClick={open}></div>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.area} onClick={changeSelected}>
       <div className={`${styles.border} ${selected}`}>
-        <div className={styles.door}>
-          <div className={styles.number}>{door.number}</div>
-          <div className={styles.doorknob}></div>
-        </div>
+        {door.opened ? false : renderDoor()}
       </div>
 
       <div className={styles.botton}></div>
