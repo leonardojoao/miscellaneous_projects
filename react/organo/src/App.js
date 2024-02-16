4fadf421ea15d7329dcf1379c8330d6c52f8c641
Leonardo Joao <leonardorsjoao@gmail.com>
2024-02-16 18:39:59 -0300

    [react][organo] Add create team feature

--- react/organo/src/App.js
+++ react/organo/src/App.js
@@ -219,8 +219,13 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState(inicial)
 
-  function deletarColaborador() {
-    console.log('Deletar colaborador')
+  function deletarColaborador(id) {
+    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id))
+  }
+
+  function cadastrarTime(novoTime) {
+    setTimes([ ...times, { ...novoTime, id: uuidv4() }])
+
   }
 
   function mudarCorDoTime(id, cor) {
@@ -235,7 +240,7 @@ function App() {
   return (
     <div>
       <Banner />
-      <Formulario times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
+      <Formulario cadastrarTime={cadastrarTime} times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
       <section className="times">
         <h1>Minha organização</h1>
         {times.map((time, indice) => <Time key={indice} time={time} colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} aoDeletar={deletarColaborador} mudarCor={mudarCorDoTime}/>)}
