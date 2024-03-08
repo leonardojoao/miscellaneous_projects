06e632e24be1ed8e8f855040b546477a7cf43192
Leonardo Joao <leonardorsjoao@gmail.com>
2024-03-08 15:44:16 -0300

    [react][task_time] Create function to select task

--- react/task_time/src/components/List/Item/index.tsx
+++ react/task_time/src/components/List/Item/index.tsx
@@ -2,9 +2,25 @@ import { ITask } from "../../../types/ITask";
 
 import style from "./../List.module.scss";
 
-export function Item({ name, time, selected, completed, id }: ITask) {
+interface Props extends ITask {
+  selectTask: (selectedTask: ITask) => void;
+}
+
+export function Item({
+  name,
+  time,
+  selected,
+  completed,
+  id,
+  selectTask,
+}: Props) {
   return (
-    <li className={style.item}>
+    <li
+      className={`${style.item} ${selected ? style.itemSelected : ""} ${
+        completed ? style.itemFinished : ""
+      }`}
+      onClick={() => selectTask({ name, time, selected, completed, id })}
+    >
       <h3>{name}</h3>
       <span>{time}</span>
     </li>
