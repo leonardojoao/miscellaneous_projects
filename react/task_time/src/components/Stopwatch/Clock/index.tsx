c7d4eea7497401d9148c56de94b336e5278cf270
Leonardo Joao <leonardorsjoao@gmail.com>
2024-03-08 18:46:00 -0300

    [react][task_time] Add the time of the selected task on the clock

--- react/task_time/src/components/Stopwatch/Clock/index.tsx
+++ react/task_time/src/components/Stopwatch/Clock/index.tsx
@@ -1,6 +1,9 @@
 import style from "./Clock.module.scss";
+// import { timeToSeconds } from "../../../common/utils/time";
+import { timeToSeconds } from "../../../common/utils/time";
 
 export function Clock() {
+  console.log("convertion", timeToSeconds("10:10:00"));
   return (
     <>
       <span className={style.numberClock}>0</span>
