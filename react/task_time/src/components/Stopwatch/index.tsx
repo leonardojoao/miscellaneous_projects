2d697bb6536d9f6f90a0a509b90744b4976ef69e
Leonardo Joao <leonardorsjoao@gmail.com>
2024-03-08 18:46:00 -0300

    [react][task_time] Add the time of the selected task on the clock

--- react/task_time/src/components/Stopwatch/index.tsx
+++ react/task_time/src/components/Stopwatch/index.tsx
@@ -1,16 +1,33 @@
-import Button from '../Button';
-import { Clock } from './Clock';
+import { useEffect, useState } from "react";
 
-import style from './Stopwatch.module.scss';
+import Button from "../Button";
+import { Clock } from "./Clock";
+import { ITask } from "../../types/ITask";
 
-export function Stopwatch() {
+import style from "./Stopwatch.module.scss";
+import { timeToSeconds } from "../../common/utils/time";
+
+interface Props {
+  selected: ITask | undefined;
+}
+
+export function Stopwatch({ selected }: Props) {
+  const [time, setTime] = useState<Number>();
+
+  useEffect(() => {
+    if(selected?.time) {
+      setTime(timeToSeconds(selected.time));
+    }
+  }, [selected])
+
   return (
     <div className={style.stopwatch}>
       <p className={style.title}>Choose a card and start a timer</p>
+      Time: {time !== undefined ? time.toString() : ''}
       <div className={style.clockWrapper}>
         <Clock />
       </div>
-      <Button name='Start'/>
+      <Button name="Start" />
     </div>
   );
 }
