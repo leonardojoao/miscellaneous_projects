ec64c0beb81c00c073b6d177137b206c7d4c2ef5
Leonardo Joao <leonardorsjoao@gmail.com>
2024-05-11 11:28:33 -0300

    [react][online_sales] Create modules login

---
+++ ts/online_sales/frontend/src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,7 @@
+import { BackgroundImage } from '../styles/loginScreen.styles';
+
+const LoginScreen = () => {
+  return <BackgroundImage src="./../../../../public/backgroundLogin.svg" />;
+};
+
+export default LoginScreen;
