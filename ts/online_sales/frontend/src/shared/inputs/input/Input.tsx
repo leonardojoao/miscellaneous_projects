7733632d549e276326f27108265245b174c4c1af
Leonardo Joao <leonardorsjoao@gmail.com>
2024-05-11 20:11:48 -0300

    [react][online_sales] Add margin property in input global component

--- ts/online_sales/frontend/src/shared/inputs/input/Input.tsx
+++ ts/online_sales/frontend/src/shared/inputs/input/Input.tsx
@@ -4,10 +4,11 @@ import { BoxInput, TitleInput } from './input.styles';
 
 interface InputProps extends InputPropsAntd {
   title?: string;
+  margin?: string;
 }
 
-const Input = ({ title, ...props }: InputProps) => (
-  <BoxInput>
+const Input = ({ title, margin, ...props }: InputProps) => (
+  <BoxInput style={{ margin }}>
     {title && <TitleInput>{title}</TitleInput>}
 
     <InputAntd {...props} />
