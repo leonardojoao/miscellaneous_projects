550e9f367fefb8a151b887ed0b3ead271b28d831
Leonardo Joao <leonardorsjoao@gmail.com>
2024-05-14 16:15:31 -0300

    [react][online_sales] Add router to login

--- ts/online_sales/frontend/src/main.tsx
+++ ts/online_sales/frontend/src/main.tsx
@@ -1,12 +1,16 @@
 import './index.css';
 
+import type { Router as RemixRouter } from '@remix-run/router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App.tsx';
+import { loginRoutes } from './modules/login/routes';
+
+const router: RemixRouter = createBrowserRouter([...loginRoutes]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
