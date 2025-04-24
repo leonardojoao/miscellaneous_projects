dac7a4f2e861dbea0e7f69971a0a293a4c6ec73d
Leonardo Joao <leonardorsjoao@gmail.com>
2025-04-24 08:47:00 -0300

    [ts][w_promo_bot] Add enums to standardize messages in firebase

---
+++ ts/whatsapp_promotion_bot/src/firebase/messages.enum.ts
@@ -0,0 +1,10 @@
+export enum Messages {
+  SAVE_SUCCESS = 'Dados salvos com sucesso',
+  SAVE_ERROR = 'Erro ao salvar dados',
+  FETCH_ERROR = 'Erro ao buscar o dado',
+  FETCH_ALL_ERROR = 'Erro ao buscar os dados',
+  UPDATE_SUCCESS = 'Dados atualizados com sucesso',
+  UPDATE_ERROR = 'Erro ao atualizar dados',
+  NOT_FOUND = 'Nenhum dado encontrado',
+  DB_EMPTY = 'Nenhum dado encontrado na base de dados',
+}
