414fa9d65c8c7035dd69e01e10fbee2f374c3e06
Leonardo Joao <leonardorsjoao@gmail.com>
2025-09-04 17:32:10 -0300

    [ts][video_generation] Remove sensitive information logging from AuthService constructor

--- ts/video_generator/src/auth/auth.service.ts
+++ ts/video_generator/src/auth/auth.service.ts
@@ -6,10 +6,6 @@ export class AuthService {
   private oauth2Client;
 
   constructor() {
-    console.log('CLIENT_ID:', process.env.YT_CLIENT_ID);
-    console.log('CLIENT_SECRET:', process.env.YT_CLIENT_SECRET);
-    console.log('REDIRECT_URI:', process.env.YT_REDIRECT_URI);
-
     this.oauth2Client = new google.auth.OAuth2(
       process.env.YT_CLIENT_ID,
       process.env.YT_CLIENT_SECRET,
